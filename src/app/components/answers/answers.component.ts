72eda3811ac613aa70a9a734ed1dffece30d629f
magdalenaes <magdalena.sremska@gmail.com>
2021-01-17 21:33:48 +0100

    answer component

--- src/app/components/answers/answers.component.ts
+++ src/app/components/answers/answers.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { WordType } from 'src/app/data/models';
 
 @Component({
   selector: 'app-answers',
   templateUrl: './answers.component.html',
-  styleUrls: ['./answers.component.css']
+  styleUrls: ['./answers.component.css'],
 })
 export class AnswersComponent implements OnInit {
+  @Input() title: string;
+  @Input() words: WordType[];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
